4aef3a0e11095790f80167fb32937c978be52144
antatranta <a_tran38@u.pacific.edu>
2019-03-27 19:09:24 -0700

    Map and character file loader to preload files in a list

---
+++ src/file_loader.py
@@ -0,0 +1,33 @@
+""" All utilities and class for map preloading into a list """
+#pylint: disable=too-few-public-methods
+import os
+
+
+class MapFileLoader:
+    """ Class to load the map files (png, jpg, etc) """
+
+    def __init__(self):
+        self.map_files = []
+        self.map_path = os.path.join(".", "assets", "images", "maps")
+
+    def load_file(self):
+        """ Loads the map files into map_files list """
+
+        for map_file in os.listdir(self.map_path):
+            if map_file.endswith(".png") or map_file.endswith(".jpg"):
+                self.map_files.append(os.path.join(self.map_path, map_file))
+
+
+class CharacterFileLoader:
+    """ Class to load the character files (png, jpg, etc) """
+
+    def __init__(self):
+        self.characters = []
+        self.character_path = os.path.join(".", "assets", "images", "characters")
+
+    def load_character_files(self):
+        """ Loads the character files into characters list """
+
+        for map_file in os.listdir(self.character_path):
+            if map_file.endswith(".png") or map_file.endswith(".jpg"):
+                self.characters.append(os.path.join(self.character_path, map_file))
