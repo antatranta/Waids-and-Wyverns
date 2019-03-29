adf5a26fddd0d1008797aa2965bafca3124325eb
jeffr <j_wu35@u.pacific.edu>
2019-03-28 20:23:01 -0700

    Window stays open with black background. Tests can be run on it if needed

--- src/initiative_graphical.py
+++ src/initiative_graphical.py
@@ -8,6 +8,7 @@ import pygame.locals
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 BACKGROUND_COLOR = (155, 155, 155)
+run = True
 
 class GraphicalInitiativeTracker:
     """Class to Start Graphical Initiative Tracker"""
@@ -18,19 +19,30 @@ class GraphicalInitiativeTracker:
 
     def start_game(self):
         """Start Initiative Graphical"""
+        pygame.init()
         screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        screen.fill(BACKGROUND_COLOR)
 
-        while True:
-            if self.end_game:
-                self.quit()
+        while run:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
 
-            self._handle_events()
+            #self._handle_events()
 
             # Clear screen
             screen.fill(BACKGROUND_COLOR)
+
+    def decrement_health(self, health):
+        self.input = input("How much damage was dealt?")
+        self.health = self.health - self.input
 
     @classmethod
     def quit(cls):
         """Exit Initiative Graphical"""
         pygame.quit()
         sys.exit()
+
+if __name__ == "__main__":
+    ig = GraphicalInitiativeTracker("Reeeee", 14, 10)
+    ig.start_game()
