aa920250d85f47fb8cce2e6471b414d688bcec94
Alex Pelavin <ajpele@gmail.com>
2019-03-30 19:25:10 -0700

    Add update method to gui.screen.Screen

--- src/gui/screen.py
+++ src/gui/screen.py
@@ -26,6 +26,7 @@ class Screen(ABC):
         while self.running:
             screen.fill(self.background_color)
 
+            self._update()
             self._handle_events(pygame.event.get())
             self._draw(screen)
 
@@ -47,6 +48,10 @@ class Screen(ABC):
     def _draw(self, screen):
         """Draws this screen."""
         raise NotImplementedError
+
+    def _update(self):
+        # pylint: disable=no-self-use
+        pass
 
 
 class ComponentScreen(Screen):
