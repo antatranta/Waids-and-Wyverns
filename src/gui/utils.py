e6e7dff304d17ae0134ec71c15de218a79895ac3
Alex Pelavin <ajpele@gmail.com>
2019-04-02 01:51:02 -0700

    Create DraggableMixin class

--- src/gui/utils.py
+++ src/gui/utils.py
@@ -1,5 +1,39 @@
 """Utilities for GUI related actions."""
 
+import pygame
+
+class DraggableMixin:
+    """
+    Mixin Class to allow objects to be draggable.
+
+    Subclasses must have self.rect which is a pygame.Rect
+    which will be used to know if the object has been clicked.
+    """
+
+    def __init__(self, pos):
+        self._draggable_selected = False
+        self._draggable_offset = (0, 0)
+        self.pos = pos
+
+    def handle_events(self, events):
+        """Handle events for this element."""
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONUP:
+                self._draggable_selected = False
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if self.rect.collidepoint(event.pos):
+                    self._draggable_selected = True
+                    self._draggable_offset = (self.pos[0] - event.pos[0],
+                                              self.pos[1] - event.pos[1])
+
+            elif event.type == pygame.MOUSEMOTION:
+                if self._draggable_selected:
+                    self.pos = (self._draggable_offset[0] + event.pos[0],
+                                self._draggable_offset[1] + event.pos[1])
+                else:
+                    self._draggable_selected = False
+
 
 def draw_text(screen, font, text, pos, color=(0, 0, 0), *,
               background=None, center=False, split_char='\n'):
--- src/map_screen.py
+++ src/map_screen.py
@@ -4,6 +4,7 @@ import pygame
 
 from .file_loader import MapFileLoader, CharacterFileLoader
 from .gui.screen import Screen
+from .gui.utils import DraggableMixin
 
 
 class MapAndCharacterScreen(Screen):
@@ -14,7 +15,7 @@ class MapAndCharacterScreen(Screen):
         self.map_width = super().screen_width
         self.map_height = super().screen_height
         self.map_image = self._load_map()
-        self.character_rect = []
+        self._characters = []
         self.character_images = self._load_characters()
         self.character_dragging = False
 
@@ -29,8 +30,8 @@ class MapAndCharacterScreen(Screen):
 
     def _draw_characters(self, screen):
         """ Draws the character to the screen """
-        for i, character in enumerate(self.character_images):
-            screen.blit(character, self.character_rect[i])
+        for character in self._characters:
+            character.draw(screen)
 
     def _load_map(self):
         """ Load the maps into an array of Surface (pygame) of images"""
@@ -47,28 +48,15 @@ class MapAndCharacterScreen(Screen):
 
         character_images = []
         for i, character_file_location in enumerate(character_files):
-            character_images.append(pygame.image.load(character_file_location))
-            character_images[i] = pygame.transform.smoothscale(character_images[i], (100, 100))
+            img = pygame.image.load(character_file_location)
+            img = pygame.transform.smoothscale(img, (100, 100))
 
-            self.character_rect.append(character_images[i].get_rect())
-            self.character_rect[i] = self.character_rect[i].move(i * 100, 0)
+            self._characters.append(_Character(img, (i * 100, 0)))
 
         return character_images
 
     def _update(self):
         """ Updates the screen when click and dragging """
-
-        # Collision detection #
-        # character_rect_collision = self.character_rect
-
-        # for rect in self.character_rect:
-        #    if rect.collidepoint(pygame.mouse.get_pos()):
-        #        for rect_collision in character_rect_collision:
-        #            if character_rect_collision.index(rect_collision)
-        #               != self.character_rect.index(rect):
-        #                if rect.colliderect(rect_collision):
-        #                    rect.move_ip(rect.x + 50, rect.y + 50)
-
         if self.character_dragging:
             for rect in self.character_rect:
                 rect.center = pygame.mouse.get_pos()
@@ -77,17 +65,27 @@ class MapAndCharacterScreen(Screen):
         """ Handle events in maps """
         super()._handle_events(events)
 
-        for event in events:
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    # iterate through the character rects to see which collides with mouse
-                    for rect in self.character_rect:
-                        if rect.collidepoint(event.pos):
-                            self.character_dragging = True
-            elif event.type == pygame.MOUSEBUTTONUP:
-                if event.button == 1:
-                    self.character_dragging = False
+        for character in self._characters:
+            character.handle_events(events)
 
+        for event in events:
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_ESCAPE:
                     self.close()
+
+class _Character(DraggableMixin):
+
+    def __init__(self, img, pos=(0, 0)):
+        DraggableMixin.__init__(self, pos)
+        self.img = img
+
+    @property
+    def rect(self):
+        """Rect of the character"""
+        rect = self.img.get_rect()
+        rect.topleft = self.pos
+        return rect
+
+    def draw(self, screen):
+        """Draw this element to screen."""
+        screen.blit(self.img, self.pos)
