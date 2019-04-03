742975b85c4795add26131a9caeb016badc51549
Anthony Helou <anthonyhelou44@gmail.com>
2019-04-03 12:06:21 -0700

    more pylint refactoring

--- src/dice_roller_screen.py
+++ src/dice_roller_screen.py
@@ -7,10 +7,10 @@ from .gui.screen import Screen
 
 class DiceRollerScreen(Screen):
     """Class to start graphical dice roller"""
-    def __init__(self, screen):
+
+    def __init__(self):
         super().__init__()
-        self.screen = screen
         self._font = pygame.font.SysFont('comicsansms', 18)
 
     def _draw(self, screen):
-        self.screen = screen
+        """draw function to draw necessary objects on screen"""
