8b6acce3efb896c9397391f8997ad57dac072d47
jeffr <j_wu35@u.pacific.edu>
2019-04-03 20:45:30 -0700

    Added modifier to dice rolls and adv/disadv func rules for rerolls.

--- src/dice.py
+++ src/dice.py
@@ -1,7 +1,6 @@
 """ All utilities and classes for dice functions """
-
+"""Goal: https://www.wizards.com/dnd/dice/dice.htm"""
 import random
-
 
 class DiceRoll:
     """ Class to create dice """
@@ -18,6 +17,45 @@ class DiceRoll:
         while count < times:
             print(self.check_dice_type(dice_type))
             count += 1
+
+    def _modifier(self, choice, total):
+        "Increments/Decrements dice as needed"
+        positive = True
+        negative = False
+        mod_num = input("Enter modifier value")
+
+        #Positive vs Negative selection button
+        if (self.choice == positive):
+            #find num value then add to dice roll
+            total += mod_num
+            return total
+
+        elif (self.choice == negative):
+            total += mod_num
+            return total
+
+    def _advantage_disadvantage(self, situation):
+        "Advantage rolls same dice again and picks larger, disadvantage rolls again, picks lower"
+        "Rules: https://5thsrd.org/rules/advantage_and_disadvantage/"
+        advantage = True
+        disadvantage = False
+        compare1 = 0
+        compare2 = 0
+        if situation == advantage:
+            "Use the same dice type that is picked, i.e roll d4 twice"
+            compare1 = self.check_dice_type(self.dice_type)
+            compare2 = self.check_dice_type(self.dice_type)
+            if compare1 > compare2:
+                return compare1
+            else:
+                return compare2
+        elif situation == disadvantage:
+            compare1 = self.check_dice_type(self.dice_type)
+            compare2 = self.check_dice_type(self.dice_type)
+            if compare1 > compare2:
+                return compare2
+            else:
+                return compare1
 
     @staticmethod
     def check_dice_type(self, dice_type):
@@ -45,3 +83,5 @@ class DiceRoll:
         else:
             print(error_message)
 
+if __name__ == "__main__":
+    DiceRoll.check_dice_type()
