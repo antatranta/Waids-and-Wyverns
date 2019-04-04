bd74672ee367b8785da70d473d957d14d1e2efe8
Antonio Tran <atran5096@gmail.com>
2019-04-04 15:31:04 -0700

    just some edits to box placement

--- src/dice_roller_screen.py
+++ src/dice_roller_screen.py
@@ -12,13 +12,16 @@ class DiceRollerScreen(Screen):
 
     def __init__(self):
         super().__init__()
-        self._d4box = TextBox((0, 0), (50, 30), allowed=NUMERIC_KEYS, center=True)
-        self._d6box = TextBox((0, 0), (50, 30), allowed=NUMERIC_KEYS, center=True)
-        self._d8box = TextBox((0, 0), (50, 30), allowed=NUMERIC_KEYS, center=True)
-        self._d10box = TextBox((0, 0), (50, 30), allowed=NUMERIC_KEYS, center=True)
-        self._d100box = TextBox((0, 0), (50, 30), allowed=NUMERIC_KEYS, center=True)
-        self._d12box = TextBox((0, 0), (50, 30), allowed=NUMERIC_KEYS, center=True)
-        self._d20box = TextBox((0, 0), (50, 30), allowed=NUMERIC_KEYS, center=True)
+
+        self._d4box = TextBox((0, 0), (50, 30), "0", allowed=NUMERIC_KEYS, center=True)
+        self._d6box = TextBox((0, 0), (50, 30), "0", allowed=NUMERIC_KEYS, center=True)
+        self._d8box = TextBox((0, 0), (50, 30), "0", allowed=NUMERIC_KEYS, center=True)
+        self._d10box = TextBox((0, 0), (50, 30), "0", allowed=NUMERIC_KEYS, center=True)
+        self._d100box = TextBox((0, 0), (50, 30), "0", allowed=NUMERIC_KEYS, center=True)
+        self._d12box = TextBox((0, 0), (50, 30), "0", allowed=NUMERIC_KEYS, center=True)
+        self._d20box = TextBox((0, 0), (50, 30), "0", allowed=NUMERIC_KEYS, center=True)
+
+        self._roll_button = pygame.Rect(0, 0, 0, 0)
         self._font = pygame.font.SysFont('comicsansms', 18)
 
     def _draw(self, screen):
@@ -27,7 +30,7 @@ class DiceRollerScreen(Screen):
 
     def _handle_events(self, events):
         super()._handle_events(events)
-
+        
         self._d4box.handle_events(events)
         self._d6box.handle_events(events)
         self._d8box.handle_events(events)
@@ -38,12 +41,12 @@ class DiceRollerScreen(Screen):
 
         for event in events:
             if event.type == pygame.MOUSEBUTTONUP:
-                print("mouse click")
-                print(pygame.mouse.get_pos())
+                if self._roll_button.collidepoint(pygame.mouse.get_pos()):
+                    print("roll")
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_RETURN:
-                    self._add_character()
+                    self._roll_result(1, 2, 3, 4, 5, 6)
                 if event.key == pygame.K_ESCAPE:
                     self.close()
 
@@ -56,6 +59,9 @@ class DiceRollerScreen(Screen):
 
         return True
 
+    def _roll_result(d4_val, d6_val, d8_val, d10_val, d12_val, d20_val, d100_val):
+        print("test")
+
     def _draw_input(self, screen, pos):
         self._d4box.draw(screen)
         self._d6box.draw(screen)
@@ -66,30 +72,38 @@ class DiceRollerScreen(Screen):
         self._d20box.draw(screen)        
 
         d4_width, _ = draw_text(screen, self._font, "# of D4 dice: ", (10, 0))
-        self._d4box.rect.left = 100
+        self._d4box.rect.left = d4_width + 10
 
         d6_width, _ = draw_text(screen, self._font, "# of D6 dice: ", (10, 50))
-        self._d6box.rect.left = 100
+        self._d6box.rect.left = d6_width + 10
         self._d6box.rect.bottom = 75
 
         d8_width, _ = draw_text(screen, self._font, "# of D8 dice: ", (10, 100))
-        self._d8box.rect.left = 100
+        self._d8box.rect.left = d8_width + 10
         self._d8box.rect.bottom = 125
 
         d10_width, _ = draw_text(screen, self._font, "# of D10 dice: ", (10, 150))
-        self._d10box.rect.left = 100
+        self._d10box.rect.left = d10_width + 10
         self._d10box.rect.bottom = 175
 
         d12_width, _ = draw_text(screen, self._font, "# of D12 dice: ", (10, 200))
-        self._d12box.rect.left = 100
+        self._d12box.rect.left = d12_width  + 10
         self._d12box.rect.bottom = 225
 
         d20_width, _ = draw_text(screen, self._font, "# of D20 dice: ", (10, 250))
-        self._d20box.rect.left = 100
+        self._d20box.rect.left = d20_width + 10
         self._d20box.rect.bottom = 275
 
         d100_width, _ = draw_text(screen, self._font, "# of D100 dice: ", (10, 300))
-        self._d100box.rect.left = 100
+        self._d100box.rect.left = d100_width + 10
         self._d100box.rect.bottom = 325
 
+        self._roll_button = pygame.Rect(250, 400, 100, 50)
+        pygame.draw.rect(screen, (255, 255, 255), self._roll_button)
+
         add_color = (0, 0, 0) if self._valid_input() else (200, 200, 200)
+        draw_text(screen, self._font, "Roll", self._roll_button.center, add_color, center=True)
+    
+    def _roll_result(d4_val, d6_val, d8_val, d10_val, d12_val, d20_val, d100_val):
+        if self._valid_input():
+            print("valid")
