6bb85a3896dbf9574a74c91b30046be738edc1cb
antatranta <a_tran38@u.pacific.edu>
2019-04-03 22:18:54 -0700

    GUI implementation of sound/music player

--- src/sound_screen.py
+++ src/sound_screen.py
@@ -4,7 +4,7 @@ import pygame
 
 from .file_loader import MusicFileLoader, SoundFileLoader
 from .gui.screen import Screen
-from .gui.utils import draw_text
+from .gui.utils import draw_text, Button
 
 
 class SoundPlayerScreen(Screen):
@@ -15,8 +15,27 @@ class SoundPlayerScreen(Screen):
 
     def __init__(self):
         super().__init__()
-        self._music_function_buttons = []
-        self._sound_function_buttons = []
+        box_height = (super().screen_height * 0.05)
+        box_width = (super().screen_width * 0.70)
+        function_width_size = (super().screen_width - box_width) / 3
+        button_size = (function_width_size, box_height)
+
+        self._music_box = pygame.Rect(0, 0, box_width, box_height)
+        self._sound_box = pygame.Rect(0, box_height, box_width, box_height)
+        self._music_function_buttons = [
+            Button("|>", (self._music_box.right, 0),
+                   button_size, self._play_music),
+            Button("||", (self._music_box.right + function_width_size, 0),
+                   button_size, self._pause_music),
+            Button("X", (self._music_box.right + 2*function_width_size, 0),
+                   button_size, self._stop_music)]
+        self._sound_function_buttons = [
+            Button("|>", (self._sound_box.right, box_height),
+                   button_size, self._play_sound),
+            Button("||", (self._sound_box.right + function_width_size, box_height),
+                   button_size, self._pause_sound),
+            Button("X", (self._sound_box.right + 2*function_width_size, box_height),
+                   button_size, self._stop_sound)]
 
     def _load_music(self):
         pass
@@ -26,63 +45,48 @@ class SoundPlayerScreen(Screen):
 
     def _draw(self, screen):
         self._draw_music(screen, "placeholder.mp3")
+        for music_button in self._music_function_buttons:
+            music_button.draw(screen)
+
         self._draw_sound(screen, "placeholder.wav")
+        for sound_button in self._sound_function_buttons:
+            sound_button.draw(screen)
 
     def _draw_music(self, screen, music_name):
-        music_box_height = super().screen_height * 0.05
-        music_box = pygame.Rect(0, 0,
-                                super().screen_width * 0.70, music_box_height)
-
-        function_buttons_width = (super().screen_width - music_box.width) / 3
-        play_button = pygame.Rect(music_box.right, music_box.top,
-                                  function_buttons_width, music_box_height)
-        pause_button = pygame.Rect(play_button.right, play_button.top,
-                                   function_buttons_width, music_box_height)
-        stop_button = pygame.Rect(pause_button.right, pause_button.top,
-                                  function_buttons_width, music_box_height)
-
-        pygame.draw.rect(screen, (255, 255, 255), music_box)
-        pygame.draw.rect(screen, (0, 255, 0), play_button)
-        pygame.draw.rect(screen, (255, 165, 0), pause_button)
-        pygame.draw.rect(screen, (255, 0, 0), stop_button)
-
-        draw_text(screen, self._font, music_name, music_box.center, center=True)
-        draw_text(screen, self._font, "|>", play_button.center, center=True)
-        draw_text(screen, self._font, "||", pause_button.center, center=True)
-        draw_text(screen, self._font, "X", stop_button.center, center=True)
+        pygame.draw.rect(screen, (255, 255, 255), self._music_box, 2)
+        draw_text(screen, self._font, music_name, self._music_box.center, center=True)
 
     def _draw_sound(self, screen, sound_name):
-        music_box_height = super().screen_height * 0.05
-        sound_box = pygame.Rect(0, music_box_height,
-                                super().screen_width * 0.70, music_box_height)
-
-        function_buttons_width = (super().screen_width - sound_box.width) / 3
-        play_button = pygame.Rect(sound_box.right, sound_box.top,
-                                  function_buttons_width, music_box_height)
-        pause_button = pygame.Rect(play_button.right, play_button.top,
-                                   function_buttons_width, music_box_height)
-        stop_button = pygame.Rect(pause_button.right, pause_button.top,
-                                  function_buttons_width, music_box_height)
-
-        pygame.draw.rect(screen, (255, 255, 255), sound_box)
-        pygame.draw.rect(screen, (0, 255, 0), play_button)
-        pygame.draw.rect(screen, (255, 165, 0), pause_button)
-        pygame.draw.rect(screen, (255, 0, 0), stop_button)
-
-        draw_text(screen, self._font, sound_name, sound_box.center, center=True)
-        draw_text(screen, self._font, "|>", play_button.center, center=True)
-        draw_text(screen, self._font, "||", pause_button.center, center=True)
-        draw_text(screen, self._font, "X", stop_button.center, center=True)
+        pygame.draw.rect(screen, (255, 255, 255), self._sound_box, 2)
+        draw_text(screen, self._font, sound_name, self._sound_box.center, center=True)
 
     def _play_music(self):
-        pass
+        print("Playing music!")
 
     def _play_sound(self):
-        pass
+        print("Playing sound!")
+
+    def _pause_music(self):
+        print("Paused music!")
+
+    def _pause_sound(self):
+        print("Paused sound!")
+
+    def _stop_music(self):
+        print("Stopped music!")
+
+    def _stop_sound(self):
+        print("Stopped sound!")
 
     def _handle_events(self, events):
         """ Handle events in sound player """
         super()._handle_events(events)
+
+        for music_button in self._music_function_buttons:
+            music_button.handle_events(events)
+
+        for sound_button in self._sound_function_buttons:
+            sound_button.handle_events(events)
 
         for event in events:
             if event.type == pygame.KEYUP:
