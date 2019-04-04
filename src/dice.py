f8c2737a5890557f64d1cf99b7bc3d3c8b4ab86a
Alex Pelavin <ajpele@gmail.com>
2019-04-03 21:30:50 -0700

    Lint

--- src/dice.py
+++ src/dice.py
@@ -1,5 +1,5 @@
-""" All utilities and classes for dice functions """
-"""Goal: https://www.wizards.com/dnd/dice/dice.htm"""
+""" All utilities and classes for dice functions"""
+# Goal: https://www.wizards.com/dnd/dice/dice.htm
 import random
 
 class DiceRoll:
@@ -13,75 +13,83 @@ class DiceRoll:
     def _roll(self, times, dice_type):
         """ Rolls dice x amount of times """
         count = 0
-        total = 0
+        # total = 0
         while count < times:
             print(self.check_dice_type(dice_type))
             count += 1
 
-    def _modifier(self, choice, total):
-        "Increments/Decrements dice as needed"
+    @classmethod
+    def _modifier(cls, choice, total):
+        # Increments/Decrements dice as needed
         positive = True
         negative = False
         mod_num = input("Enter modifier value")
 
         #Positive vs Negative selection button
-        if (self.choice == positive):
+        if choice == positive:
             #find num value then add to dice roll
             total += mod_num
-            return total
 
-        elif (self.choice == negative):
+        if choice == negative:
             total += mod_num
-            return total
+
+        return total
 
     def _advantage_disadvantage(self, situation):
-        "Advantage rolls same dice again and picks larger, disadvantage rolls again, picks lower"
-        "Rules: https://5thsrd.org/rules/advantage_and_disadvantage/"
+        """Advantage rolls same dice again and picks larger,
+        disadvantage rolls again, picks lower"""
+        # Rules: https://5thsrd.org/rules/advantage_and_disadvantage/
         advantage = True
         disadvantage = False
+
         compare1 = 0
         compare2 = 0
+
         if situation == advantage:
-            "Use the same dice type that is picked, i.e roll d4 twice"
+            # Use the same dice type that is picked, i.e roll d4 twice
             compare1 = self.check_dice_type(self.dice_type)
             compare2 = self.check_dice_type(self.dice_type)
             if compare1 > compare2:
                 return compare1
-            else:
-                return compare2
-        elif situation == disadvantage:
+            return compare2
+
+        if situation == disadvantage:
             compare1 = self.check_dice_type(self.dice_type)
             compare2 = self.check_dice_type(self.dice_type)
             if compare1 > compare2:
                 return compare2
-            else:
-                return compare1
+            return compare1
+        return None # this function should always return SOMETHING
 
     @staticmethod
-    def check_dice_type(self, dice_type):
+    def check_dice_type(dice_type):
+        """TODO"""
         list_dice_types = ["d4", "d6", "d8", "d10", "d12",
                            "d20", "d100"]
         error_message = "Dice does not exist"
+        value = None
 
         if dice_type in list_dice_types:
             if dice_type == "d4":
-                return random.randint(1, 4)
+                value = random.randint(1, 4)
             elif dice_type == "d6":
-                return random.randint(1, 6)
+                value = random.randint(1, 6)
             elif dice_type == "d8":
-                return random.randint(1, 8)
+                value = random.randint(1, 8)
             elif dice_type == "d10":
-                return random.randint(1, 10)
+                value = random.randint(1, 10)
             elif dice_type == "d12":
-                return random.randint(1, 12)
+                value = random.randint(1, 12)
             elif dice_type == "d20":
-                return random.randint(1, 20)
+                value = random.randint(1, 20)
             elif dice_type == "d100":
-                return random.randint(1, 100)
+                value = random.randint(1, 100)
             else:
                 print("TEST")
         else:
             print(error_message)
+
+        return value
 
 #if __name__ == "__main__":
     #DiceRoll.check_dice_type()
