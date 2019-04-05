e665402366fdd52e19185557bf5744c40261a752
Seth <s_kith@u.pacific.edu>
2019-04-04 21:56:27 -0700

    Temp unit test for dice functions

---
+++ test/dice.py
@@ -0,0 +1,9 @@
+"""Unit test for Dice Functions"""
+import unittest
+from src.dice import roll_results, advantage_disadvantage
+
+
+class TestDiceFunctions(unittest.TestCase):
+    """Test cases for dice functions place holder for now"""
+    roll_results(1, 'd4', True, 2)
+    advantage_disadvantage(True, 'd4')
