aa7782c4b18e151be87c34889f18c4c749c79754
Seth <s_kith@u.pacific.edu>
2019-04-04 21:31:40 -0700

    Refactored dice.py

--- src/dice.py
+++ src/dice.py
@@ -3,91 +3,65 @@
 import random
 
 
-class DiceRoll:
-    """ Class to create dice """
+def roll_results(times, dice_type, choice, mod_num):
+    """Returns roll results"""
+    count = 0
+    total = 0
+    if choice:
+        total += _modifier(total, mod_num)
+    while count < times:
+        roll_dice(dice_type)
+        total += roll_dice(dice_type)
+        count += 1
+    return total
 
-    def _roll(self, times, dice_type):
-        count = 0
-        total = 0
-        while count < times:
-            roll = self.check_dice_type(dice_type)
-            print(roll)
-            total += roll
-            count += 1
-        print('Total: ' + str(total))
 
-    @classmethod
-    def _modifier(cls, choice, total):
-        # Increments/Decrements dice as needed
-        positive = True
-        negative = False
-        mod_num = input("Enter modifier value")
+def _modifier(total, mod_num):
+    """Increments/Decrements dice as needed"""
+    # mod_num = input("Enter modifier value")
+    # Positive vs Negative selection button
+    total += mod_num
+    return total
 
-        # Positive vs Negative selection button
-        if choice == positive:
-            # find num value then add to dice roll
-            total += mod_num
 
-        if choice == negative:
-            total += mod_num
+def advantage_disadvantage(advantage, dice_type):
+    """Advantage rolls same dice again and picks larger,
+    disadvantage rolls again, picks lower"""
+    # Rules: https://5thsrd.org/rules/advantage_and_disadvantage/
+    # Use the same dice type that is picked, i.e roll d4 twice
+    dice1 = roll_dice(dice_type)
+    dice2 = roll_dice(dice_type)
+    if advantage:
+        value = max(dice1, dice2)
+    else:
+        value = min(dice1, dice2)
+    return value
 
-        return total
 
-    def _advantage_disadvantage(self, situation):
-        """Advantage rolls same dice again and picks larger,
-        disadvantage rolls again, picks lower"""
-        # Rules: https://5thsrd.org/rules/advantage_and_disadvantage/
-        advantage = True
-        disadvantage = False
+def roll_dice(dice_type):
+    """Returns dice value"""
+    value = None
 
-        compare1 = 0
-        compare2 = 0
+    if dice_type == "d4":
+        value = random.randint(1, 4)
+    elif dice_type == "d6":
+        value = random.randint(1, 6)
+    elif dice_type == "d8":
+        value = random.randint(1, 8)
+    elif dice_type == "d10":
+        value = random.randint(1, 10)
+    elif dice_type == "d12":
+        value = random.randint(1, 12)
+    elif dice_type == "d20":
+        value = random.randint(1, 20)
+    elif dice_type == "d100":
+        value = random.randint(1, 100)
+    return value
 
-        if situation == advantage:
-            # Use the same dice type that is picked, i.e roll d4 twice
-            compare1 = self.check_dice_type(self.dice_type)
-            compare2 = self.check_dice_type(self.dice_type)
-            if compare1 > compare2:
-                return compare1
-            return compare2
 
-        if situation == disadvantage:
-            compare1 = self.check_dice_type(self.dice_type)
-            compare2 = self.check_dice_type(self.dice_type)
-            if compare1 > compare2:
-                return compare2
-            return compare1
-        return None # this function should always return SOMETHING
-
-    @staticmethod
-    def check_dice_type(dice_type):
-        """TODO"""
-        list_dice_types = ["d4", "d6", "d8", "d10", "d12",
-                           "d20", "d100"]
-        error_message = "Dice does not exist"
-        value = None
-
-        if dice_type in list_dice_types:
-            if dice_type == "d4":
-                value = random.randint(1, 4)
-            elif dice_type == "d6":
-                value = random.randint(1, 6)
-            elif dice_type == "d8":
-                value = random.randint(1, 8)
-            elif dice_type == "d10":
-                value = random.randint(1, 10)
-            elif dice_type == "d12":
-                value = random.randint(1, 12)
-            elif dice_type == "d20":
-                value = random.randint(1, 20)
-            elif dice_type == "d100":
-                value = random.randint(1, 100)
-            else:
-                print("TEST")
-        else:
-            print(error_message)
-
-        return value
-
+# TEMP TEST
+print("Min: " + str(advantage_disadvantage(False, 'd4')))
+print("Max: " + str(advantage_disadvantage(True, 'd4')))
+print("Roll Result: " + str(roll_results(1, 'd4', True, 2)))
 # if __name__ == "__main__":
-    # DiceRoll.check_dice_type()
+# DiceRoll.check_dice_type()
