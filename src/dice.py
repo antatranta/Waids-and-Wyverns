f9b97c1eb3169b046fe84d6ddaad46aeaed8b7be
Seth <s_kith@u.pacific.edu>
2019-04-05 19:08:15 -0700

    Renamed variables, refactor, and test functions

--- src/dice.py
+++ src/dice.py
@@ -3,21 +3,23 @@
 import random
 
 
-def roll_results(times, dice_type, choice, mod_num):
+def roll_results(times, dice_type, enable_modifier, mod_num):
     """Returns roll results"""
     count = 0
     total = 0
-    if choice:
+    rolls = []
+    if enable_modifier:
         total += _modifier(total, mod_num)
     while count < times:
-        total += roll_dice(dice_type)
+        value = roll_dice(dice_type)
+        rolls.append(value)
+        total += value
         count += 1
-    return total
+    return [rolls, mod_num, total]
 
 
 def _modifier(total, mod_num):
     """Increments/Decrements dice as needed"""
-    # mod_num = input("Enter modifier value")
     # Positive vs Negative selection button
     total += mod_num
     return total
@@ -28,12 +30,10 @@ def advantage_disadvantage(advantage, dice_type):
     disadvantage rolls again, picks lower"""
     # Rules: https://5thsrd.org/rules/advantage_and_disadvantage/
     # Use the same dice type that is picked, i.e roll d4 twice
-    dice1 = roll_dice(dice_type)
-    dice2 = roll_dice(dice_type)
     if advantage:
-        value = max(dice1, dice2)
+        value = max(roll_dice(dice_type), roll_dice(dice_type))
     else:
-        value = min(dice1, dice2)
+        value = min(roll_dice(dice_type), roll_dice(dice_type))
     return value
 
 
@@ -66,3 +66,6 @@ def roll_dice(dice_type):
 # print("Roll Result: " + str(roll_results(1, 'd4', False, 2)))
 # if __name__ == "__main__":
 # DiceRoll.check_dice_type()
+# roll_results(4, 'd4', True, -1)
+# gets item from list
+# print(roll_results(4, 'd4', False, 10).__getitem__(0))
