ca4d61377ee2b55a5ffff92c8f55670937659c7f
antatranta <a_tran38@u.pacific.edu>
2019-04-06 15:32:54 -0700

    Updated file_loader unit test for additions of music and sound

--- test/file_loader.py
+++ test/file_loader.py
@@ -1,16 +1,18 @@
 import unittest
 import os
-from src.file_loader import MapFileLoader, CharacterFileLoader
+from src.file_loader import MapFileLoader, CharacterFileLoader, MusicFileLoader, SoundFileLoader
+
 
 ASSETS = os.path.join(".", "test", "fixtures", "assets")
+
 
 class TestMapFileLoader(unittest.TestCase):
     def test_file_loader(self):
         map_path = os.path.join(ASSETS, "images", "maps")
 
-        Loader = MapFileLoader(map_path=map_path)
+        map_loader = MapFileLoader(map_path=map_path)
 
-        self.assertCountEqual(Loader.load_map_files(), [
+        self.assertCountEqual(map_loader.load_map_files(), [
             os.path.join(map_path, "my_map.jpg"),
             os.path.join(map_path, "another_map.png"),
         ])
@@ -20,9 +22,33 @@ class TestCharacterLoader(unittest.TestCase):
     def test_character_loader(self):
         character_path = os.path.join(ASSETS, "images", "characters")
 
-        CharLoader = CharacterFileLoader(character_path=character_path)
+        char_loader = CharacterFileLoader(character_path=character_path)
 
-        self.assertCountEqual(CharLoader.load_character_files(), [
+        self.assertCountEqual(char_loader.load_character_files(), [
             os.path.join(character_path, "another_character.png"),
             os.path.join(character_path, "character1.jpg"),
+        ])
+
+
+class TestMusicFileLoader(unittest.TestCase):
+    def test_music_loader(self):
+        music_path = os.path.join(ASSETS, "media", "music")
+
+        music_loader = MusicFileLoader(music_path=music_path)
+
+        self.assertCountEqual(music_loader.load_music_files(), [
+            os.path.join(music_path, "music.mp3"),
+            os.path.join(music_path, "another_music.ogg")
+        ])
+
+
+class TestSoundFileLoader(unittest.TestCase):
+    def test_sound_loader(self):
+        sound_path = os.path.join(ASSETS, "media", "sound")
+
+        sound_loader = SoundFileLoader(sound_path=sound_path)
+
+        self.assertCountEqual(sound_loader.load_sound_files(), [
+            os.path.join(sound_path, "sound.wav"),
+            os.path.join(sound_path, "another_sound.ogg")
         ])
