43cf0da06d7dd1198873752e51e1395acbe71aba
Antonio Tran <atran5096@gmail.com>
2019-04-08 15:23:58 -0700

    changes to reduce code with Alex and Anthony H's help

--- src/dice_roller_screen.py
+++ src/dice_roller_screen.py
@@ -13,6 +13,7 @@ class DiceRollerScreen(Screen):
     def __init__(self):
         super().__init__()
 
+        self._dice = [_Dice((0, 0), 4), _Dice((10, 10), 6)]
         self._dicesides = [4, 6, 8, 10, 12, 20, 100]
         self._dicemodifier = [4, 6, 8, 10, 12, 20, 100]
         self._dicenumber = []
@@ -29,36 +30,41 @@ class DiceRollerScreen(Screen):
         self._font = pygame.font.SysFont('comicsansms', 18)
 
     def _draw(self, screen):
-        """draw function to draw necessary objects on screen"""
-        self._draw_input(screen)
+        for die in self._dice:
+            die.draw(screen)
 
-    def update(self):
-        """ stuff """
-        return True
+    # def _draw(self, screen):
+    #     """draw function to draw necessary objects on screen"""
+    #     self._draw_input(screen)
 
-    def _handle_events(self, events):
-        super()._handle_events(events)
+    # def update(self):
+    #     """ stuff """
+    #     for sides in self._dice
+    #     return True
 
-        for i in range(len(self._dicesides)):
-            self._dicesides[i].handle_events(events)
+    # def _handle_events(self, events):
+    #     super()._handle_events(events)
 
-        for i in range(len(self._dicemodifier)):
-            self._dicemodifier[i].handle_events(events)
+    #     for i in range(len(self._dicesides)):
+    #         self._dicesides[i].handle_events(events)
 
-        for event in events:
-            if event.type == pygame.MOUSEBUTTONUP:
-                if self._roll_button.collidepoint(pygame.mouse.get_pos()):
-                    print("roll")
-                    # for i in range(len(self._dicesides)):
-                    #     print(self._dicesides[i].value)
+    #     for i in range(len(self._dicemodifier)):
+    #         self._dicemodifier[i].handle_events(events)
 
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_RETURN:
-                    print("roll")
-                    print(roll_results(3, "d4", False, 0))
+    #     for event in events:
+    #         if event.type == pygame.MOUSEBUTTONUP:
+    #             if self._roll_button.collidepoint(pygame.mouse.get_pos()):
+    #                 print("roll")
+    #                 # for i in range(len(self._dicesides)):
+    #                 #     print(self._dicesides[i].value)
 
-                if event.key == pygame.K_ESCAPE:
-                    self.close()
+    #         if event.type == pygame.KEYUP:
+    #             if event.key == pygame.K_RETURN:
+    #                 print("roll")
+    #                 print(roll_results(3, "d4", False, 0))
+
+    #             if event.key == pygame.K_ESCAPE:
+    #                 self.close()
 
     def _valid_input(self):
         for i in range(len(self._dicesides)):
@@ -108,75 +114,26 @@ class DiceRollerScreen(Screen):
         add_color = (0, 0, 0) if self._valid_input() else (200, 200, 200)
         draw_text(screen, self._font, "Roll", self._roll_button.center, add_color, center=True)
 
-class DiceRollerEntry:
-    """Class to start graphical dice roller entry"""
+class _Dice:
 
-    def __init__(self):
-        super().__init__()
-
-        self._dicesides = [4, 6, 8, 10, 12, 20, 100]
-        self._dicemodifier = [4, 6, 8, 10, 12, 20, 100]
-
-        for i in range(len(self._dicesides)):
-            self._dicesides[i] = TextBox((0, 0), (50, 30), "0",\
-                 allowed=NUMERIC_KEYS, center=True)
-
-        for i in range(len(self._dicemodifier)):
-            self._dicemodifier[i] = TextBox((0, 0), (50, 30), "0",\
-                 allowed=NUMERIC_KEYS, center=True)
-
+    def __init__(self, pos, sides):
+        self.pos = pos
+        self.sides = sides
         self._font = pygame.font.SysFont('comicsansms', 18)
 
+        self.label = f"# of D{sides} dice: "
+        text_width, _ = self._font.size(self.label)
+
+        self.input = TextBox((pos[0] + text_width, pos[1]), (50, 30), allowed=NUMERIC_KEYS, center=True)
+        self.modifier = TextBox((self.input.rect.right, pos[1]), (50, 30), allowed=NUMERIC_KEYS, center=True)
+
+    def draw(self, screen):
+        """Draw this component to screen."""
+        draw_text(screen, self._font, self.label, self.pos)
+        self.input.draw(screen)
+        self.modifier.draw(screen)
+
     def handle_events(self, events):
-        """Pygame handles events"""
-        super()._handle_events(events)
-
-        for i in range(len(self._dicesides)):
-            self._dicesides[i].handle_events(events)
-
-        for i in range(len(self._dicemodifier)):
-            self._dicemodifier[i].handle_events(events)
-
-        for event in events:
-            if event.type == pygame.MOUSEBUTTONUP:
-                if self._roll_button.collidepoint(pygame.mouse.get_pos()):
-                    print("roll")
-
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_RETURN:
-                    print("roll")
-                if event.key == pygame.K_ESCAPE:
-                    self.close()
-
-    def draw_input(self, screen):
-        """draw function to draw necessary objects on screen"""
-        for i in range(len(self._dicesides)):
-            self._dicesides[i].draw(screen)
-
-        dice_width = [4, 6, 8, 10, 12, 20, 100]
-        x_pos = 10
-        y_pos = 0
-        rect_bottom = 75
-
-        for i in range(len(self._dicesides)):
-            if i == 0:
-                dice_string = "# of D4 dice: "
-            elif i == 1:
-                dice_string = "# of D6 dice: "
-            elif i == 2:
-                dice_string = "# of D8 dice: "
-            elif i == 3:
-                dice_string = "# of D10 dice: "
-            elif i == 4:
-                dice_string = "# of D12 dice: "
-            elif i == 5:
-                dice_string = "# of D20 dice: "
-            elif i == 6:
-                dice_string = "# of D100 dice: "
-
-            dice_width[i], _ = draw_text(screen, self._font, dice_string, (x_pos, y_pos))
-            self._dicesides[i].rect.left = dice_width[i] + x_pos
-            y_pos += 50
-            if i > 0:
-                self._dicesides[i].rect.bottom = rect_bottom
-                rect_bottom += 50
+        """Handle events for this component."""
+        self.input.handle_events(events)
+        self.modifier.handle_events(events)
