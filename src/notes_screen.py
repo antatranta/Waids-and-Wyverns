c43ccdd6315e4a9756a3f9ed66d2f0ec43dcb8ae
Anthonyhelou <anthonyhelou44@gmail.com>
2019-04-14 16:50:05 -0700

    notes opens up notes.txt file now, deleted notes.py, not going to use it

--- src/notes_screen.py
+++ src/notes_screen.py
@@ -1,6 +1,7 @@
 """All Utilities and Classes for Notes Graphical Display"""
 
 import pygame
+import os
 
 from .gui.screen import Screen
 from .gui.textbox import TextArea
@@ -11,13 +12,12 @@ class NotesScreen(Screen):
 
     def __init__(self):
         super().__init__()
+        os.path.join("C:/Users/project2-waid/assets/media/notes/notes.txt")
+        file = open("assets/media/notes/notes.txt", "r+")
         self._textarea = TextArea((0, 0), (self.screen_width, self.screen_height),
-                                  always_selected=True, initial_value=(
-                                      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed"
-                                      "diam nonumy eirmod tempor invidunt ut labore et dolore magna"
-                                      "aliquyam erat, sed diam voluptua. At vero eos et accusam et"
-                                      "justo duo dolores et ea rebum. Stet clita kasd gubergren, no"
-                                      "sea takimata sanctus est Lorem ipsum dolor sit amet."))
+                                  always_selected=True, initial_value=file.read())
+        print(file.read())
+
 
     def _draw(self, screen):
         self._textarea.draw(screen)
