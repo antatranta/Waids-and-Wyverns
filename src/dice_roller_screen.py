10deb77a03bd7614a121b2b0079ac49edc43769b
jeffr <j_wu35@u.pacific.edu>
2019-04-15 22:04:43 -0700

    Round 2, Attempt #5 on fixing Travis issues without ignores. Completed long line issue :(

--- src/dice_roller_screen.py
+++ src/dice_roller_screen.py
@@ -86,11 +86,11 @@ class DiceRollerScreen(Screen):
                 for i, sides in enumerate(self._dicesides):
                     if int(currentline[i+1]) != 0:
                         if int(currentline[8]) != 0:
-                            self._macro_result.append(roll_results(int(currentline[i + 1]), "d" + str(sides),
-                                                                   False, int(currentline[8])))
+                            self._macro_result.append(roll_results(
+                                int(currentline[i + 1]), "d" + str(sides), False, int(currentline[8])))
                         else:
-                            self._macro_result.append(roll_results(int(currentline[i + 1]), "d" + str(sides),
-                                                                   True, int(currentline[8])))
+                            self._macro_result.append(roll_results(
+                                int(currentline[i + 1]), "d" + str(sides), True, int(currentline[8])))
                 rolls = '+'.join(str(e) for e in self._macro_result[0][0])
                 modifier = str(self._macro_result[0][1])
                 total = str(self._macro_result[0][2])
