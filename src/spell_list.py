c3e2850d26ac59b5ef095c26b7b3f387b48dfd90
Seth <s_kith@u.pacific.edu>
2019-04-29 13:42:31 -0700

    Draws spells list, search bar implemented

--- src/spell_list.py
+++ src/spell_list.py
@@ -5,6 +5,7 @@ import json
 
 from .gui.screen import Screen
 from .gui.utils import draw_text
+from .gui.textbox import TextBox
 
 
 class SpellList(Screen):
@@ -13,23 +14,23 @@ class SpellList(Screen):
     def __init__(self):
         super().__init__()
         self.spell_list = self._load_spell_list()
+        self._search_bar = TextBox((380, 400), (200, 50), "", label="Search Spell(s)")
 
     def _draw(self, screen):
-        temp = ''
+        spell = ''
         for spellsname, info in self.spell_list.items():
-            temp += spellsname + ' ' + str(info['level']) + "\n"
-        draw_text(screen, self._font, temp, (0, 0))
+            if self._search_bar.value.lower() in spellsname.lower() or self._search_bar.value == str(info['level']):
+                spell += spellsname + ' ' + str(info['level']) + "\n"
+        if self._search_bar.value:
+            draw_text(screen, self._font, spell, (0, 0))
+        self._search_bar.draw(screen)
 
     def _load_spell_list(self):
         spell_path = os.path.join(".", "assets", "spells.json")
         spell_file = json.load(open(spell_path, 'r'))
         return spell_file
 
-    def search_spell(self, spell_name):
-        """Returns searched spell"""
-        error_msg = "No spell found"
-        if spell_name in self.spell_list:
-            return self.spell_list.get(spell_name)
-        else:
-            return error_msg
+    def _handle_events(self, events):
+        super()._handle_events(events)
+        self._search_bar.handle_events(events)
 
