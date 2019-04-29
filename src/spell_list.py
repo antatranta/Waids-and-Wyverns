ff9c5da8618e03423de22de3789605e109126f6f
Seth <s_kith@u.pacific.edu>
2019-04-29 14:04:13 -0700

    Pylint should pass..

--- src/spell_list.py
+++ src/spell_list.py
@@ -1,7 +1,8 @@
 """All utilities for Spell List"""
 
-import os
 import json
+import os
+import pygame
 
 from .gui.screen import Screen
 from .gui.utils import draw_text
@@ -13,19 +14,22 @@ class SpellList(Screen):
 
     def __init__(self):
         super().__init__()
+        # pygame.
         self.spell_list = self._load_spell_list()
-        self._search_bar = TextBox((380, 400), (200, 50), "", label="Search Spell(s)")
+        self._search_bar = TextBox((380, 400), (200, 50), "", label="Filter Spell(s)")
 
     def _draw(self, screen):
         spell = ''
         for spellsname, info in self.spell_list.items():
-            if self._search_bar.value.lower() in spellsname.lower() or self._search_bar.value == str(info['level']):
+            if self._search_bar.value.lower() in spellsname.lower() \
+                    or self._search_bar.value == str(info['level']):
                 spell += spellsname + ' ' + str(info['level']) + "\n"
         if self._search_bar.value:
             draw_text(screen, self._font, spell, (0, 0))
         self._search_bar.draw(screen)
 
-    def _load_spell_list(self):
+    @staticmethod
+    def _load_spell_list():
         spell_path = os.path.join(".", "assets", "spells.json")
         spell_file = json.load(open(spell_path, 'r'))
         return spell_file
@@ -34,3 +38,7 @@ class SpellList(Screen):
         super()._handle_events(events)
         self._search_bar.handle_events(events)
 
+        for event in events:
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_ESCAPE:
+                    self.close()
